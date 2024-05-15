53485d6d94bc5e76ef3e32a308d6ab1d9d1cf27f
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-15 21:39:27 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-// import axios from 'axios'
+import axios from 'axios'
 import { useState } from 'react'
 import './App.css'
 
 const App = () => {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ccc28cfef87326fdf27adb45071461a8`
   const handleChange = (e) =>{
     setLocation(e.target.value)
   }
   const handleClick = () =>{
-    
+    axios.get(url)
+      .then((response) =>{
+        setData(response.data)
+        console.log(response.data)
+      })
   }
   return (
     <div className='container'>
@@ -19,23 +24,23 @@ const App = () => {
       </div>
       <div className="container__middle">
         <div className="container__middle--location">
-          <h1>{location}</h1>
-          <p>32°C</p>
+          <h1>{data.name}</h1>
+          {data.main ?  <p>{data.main.temp}°F</p> : null}
         </div>
-        <h4>Clear</h4>
+        {data.weather ? <h4>{data.weather[0].main}</h4> : null}
       </div>
       <div className="container__bottom">
        <div className='bottom'>
         <div className="bottom__feel">
-            <h4>31°C</h4>
+            {data.main ?  <h4>{data.main.feels_like}°F</h4> : null}
             <p>Feels Like</p>
           </div>
           <div className="bottom__humidity">
-            <h4>20%</h4>
+            {data.main ?  <h4>{data.main.humidity}%</h4> : null}
             <p>Humidity</p>
           </div>
           <div className="bottom__speed">
-            <h4>10MPH</h4>
+            {data.wind ?  <h4>{data.wind.speed}MPH</h4> : null}
             <p>Wind Speed</p>
           </div>
        </div>
